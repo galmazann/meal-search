4c7ad6a3d96a3b9431991d57c0e6d9bf4d484668
Gonzalo Almazan <galmazan@stu.southtexascollege.edu>
2024-12-01 19:10:01 -0600

    search by name is complete

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,7 +6,7 @@ export function searchMeals({ commit }, keyword) {
 
     api.get(`search.php?s=${keyword}`)
         .then(({ data }) => {
-        commit('setSearchedmeals', data.meals)
+        commit('setSearchedMeals', data.meals)
 
     })
 
